9fe709f3d649ad3e4532aab7ff7b31262d5c1963
Geechie <68406864+Geechie@users.noreply.github.com>
2022-06-02 20:25:01 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Brandon Taylor on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
