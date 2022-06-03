2bd46b1fda29390f4984e5c4f5e2b260447a1c56
Geechie <68406864+Geechie@users.noreply.github.com>
2022-06-02 20:30:02 -0500

    Start new Pinch App. Create Structure

--- Pinch/ContentView.swift
+++ Pinch/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//MARK: Property
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    
+    //MARK: Function
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    //MARK: Content
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        //MARK: Navigation View
+        NavigationView {
+            
+            ZStack {
+                Color.clear 
+                
+            } //MARK: End of ZStack
+            
+        } //MARK: End of Navigation View
     }
 }
 
